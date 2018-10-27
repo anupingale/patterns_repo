f6b043d14300b53ac0445501eacc415d1e4d14bd
Annapurna <44020027+anupingale@users.noreply.github.com>
2018-10-27 14:39:13 +0530

    Modify file execution path in run_all_test.sh.

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 set -xe
-./testScript/run_test.sh ./src/createTriangle.js ./appTestData/input/triangleInput ./appTestData/output/triangleOutput
-./testScript/run_test.sh ./src/createDiamond.js ./appTestData/input/diamondInput ./appTestData/output/diamondOutput
-./testScript/run_test.sh ./src/createRectangle.js ./appTestData/input/rectangleInput ./appTestData/output/rectangleOutput
+./testScript/run_test.sh ./createTriangle.js ./appTestData/input/triangleInput ./appTestData/output/triangleOutput
+./testScript/run_test.sh ./createDiamond.js ./appTestData/input/diamondInput ./appTestData/output/diamondOutput
+./testScript/run_test.sh ./createRectangle.js ./appTestData/input/rectangleInput ./appTestData/output/rectangleOutput
